bcf9f529c9e7a36952c4847f884f553e790a9810
linnchong <ychong@uni-bonn.de>
2024-02-21 10:28:11 +0100

    working state

--- scripts/crop_BBs.py
+++ scripts/crop_BBs.py
@@ -1,3 +1,9 @@
+"""script to crop patches for each bb
+crops patches for each bb
+size of patch must be larger than the largest bb 
+
+"""
+
 import os
 from PIL import Image, ImageOps, ImageDraw, ImageFont
 import numpy as np
@@ -7,6 +13,10 @@ import yaml
 from pathlib import Path
 from matplotlib import font_manager
 import random
+
+
+def check_isin(x, x_min, x_max):
+   return x > x_min and x < x_max
 
 
 @click.command()
@@ -64,63 +74,90 @@ def crop_bbs(images_dir, labels_dir, output_dir, num_repeats, patch_size):
         )  # for smartphone images to be oriented correctly
 
         w, h = img.size
-        coord = np.zeros((1, 5))
+        coord = np.zeros((5))
         for i, line in enumerate(f):
             l = line.split(" ")
-            coord[0, 0] = l[1]
-            coord[0, 1] = l[2]
-            coord[0, 2] = l[3]
-            coord[0, 3] = l[4][:-1]
-            coord[0, 4] = l[0]
+            coord[0] = l[1]
+            coord[1] = l[2]
+            coord[2] = l[3]
+            coord[3] = l[4][:-1]
+            coord[4] = l[0]
 
-            # crop the image
-            m = 0
-            x_min = w * coord[m, 0] - w * coord[m, 2] / 2
-            x_max = w * coord[m, 0] + w * coord[m, 2] / 2
-            y_min = h * coord[m, 1] - h * coord[m, 3] / 2
-            y_max = h * coord[m, 1] + h * coord[m, 3] / 2
+            x_c = w * coord[0]
+            y_c = h * coord[1]
+            dw = w * coord[2]
+            dh = h * coord[3]
 
-            x_c = (x_max + x_min) / 2.
-            y_c = (y_max + y_min) / 2.
-            dw = x_max - x_min
-            dh = y_max - y_min
+            x_min = x_c - dw / 2
+            x_max = x_c + dw / 2
+            y_min = y_c - dh / 2
+            y_max = y_c + dh / 2
 
+            # To make sure that the bb is not cropped out during augmentation
             max_dx = int(round((patch_size - dw) / 2.))
             max_dy = int(round((patch_size - dh) / 2.))
 
             for n in range(num_repeats):
                 dx_random = random.randint(-max_dx, max_dx+1)
                 dy_random = random.randint(-max_dy, max_dy+1)
-
+                
+                # center of crop
                 new_x_c = int(x_c + dx_random)
                 new_y_c = int(y_c + dy_random)
 
+                # new patched image crs
                 new_x_min = int(round(new_x_c - (patch_size/2)))
                 new_y_min = int(round(new_y_c - (patch_size/2)))
                 new_x_min = max(new_x_min, 0)
                 new_y_min = max(new_y_min, 0)
 
-                
-                # transform to new coords and normalise
-                new_x = (new_x_c - new_x_min) / w
-                new_y = (new_y_c - new_y_min) / h
-
-                new_str = f"{l[0]} {new_x} {new_y} {float(l[3])*w/patch_size} {float(l[4])*h/patch_size}"
-                new_filename = Path(filename).stem + f"_{new_x_c}_{new_y_c}_{patch_size}" + Path(filename).suffix
-
                 new_x_max = new_x_min + patch_size
                 new_y_max = new_y_min + patch_size
-                # TODO make sure the crop does not go out of bounds on the max side
+
+                # make sure the crop does not go out of bounds on the max side
+                if new_x_max > w:
+                    new_x_min = w - patch_size -1
+                    new_x_max = new_x_min + patch_size
+                if new_y_max > h:
+                    new_y_min = h - patch_size -1
+                    new_y_max = new_y_min + patch_size
+
+
+                new_filename = Path(filename).stem + f"_{new_x_min}_{new_y_min}_{patch_size}" + Path(filename).suffix
 
                 new_rect = [new_x_min, new_y_min, new_x_max, new_y_max]
-                import pdb; pdb.set_trace()
                 img_cropped = img.crop(new_rect)
-           
                 img_cropped.save(os.path.join(output_dir, "images", new_filename))
-                # save label txt 
-                with open(os.path.join(output_dir, "labels", Path(new_filename).stem + ".txt"), "w") as new_label_f:
-                    new_label_f.write(new_str)
 
+                # iterate over all labels to put bb in newly cropped patch
+                with open(os.path.join(labels_dir, Path(filename).stem + ".txt"), "r") as old_label_f:
+                    with open(os.path.join(output_dir, "labels", Path(new_filename).stem + ".txt"), "w") as new_label_f:
+                        for label_str in old_label_f:
+                            old_l = label_str.split(" ")  # TODO write some function to parse these strings
+                            x_c = w * float(old_l[1])
+                            y_c = h * float(old_l[2])
+                            dw = w * float(old_l[3])
+                            dh = h * float(old_l[4].strip())
+
+                            x_min = x_c - dw / 2
+                            x_max = x_c + dw / 2
+                            y_min = y_c - dh / 2
+                            y_max = y_c + dh / 2
+
+                            # check if the bounding box is in the cropped patch or not 
+                            # assumes that the bb is smaller than the patches 
+                            is_in_x = check_isin(x_min, new_x_min, new_x_max) or check_isin(x_max, new_x_min, new_x_max)
+                            is_in_y = check_isin(y_min, new_y_min, new_y_max) or check_isin(y_max, new_y_min, new_y_max)
+                            is_in = is_in_x and is_in_y 
+
+                            if is_in:
+                                # transform to new coords and normalise
+                                new_x = (int(round(x_c)) - new_x_min) / patch_size
+                                new_y = (int(round(y_c)) - new_y_min) / patch_size
+           
+                                # save label txt 
+                                new_str = f"{l[0]} {new_x} {new_y} {float(l[3])*w/patch_size} {float(l[4])*h/patch_size}\n"
+                                new_label_f.write(new_str)
 
 
 if __name__ == "__main__":
